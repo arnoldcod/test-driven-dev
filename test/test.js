d4d8643f41c727a86af70100a169f40080c83081
Arnold Uwamwezi <arnoldcod@Arnolds-MacBook-Pro.local>
2024-07-08 12:54:52 +0300

    all tests passed

--- test/test.js
+++ test/test.js
@@ -1,15 +1,35 @@
 
-// assertEqual(multiply(1, 1), 1);
+// assertEqual(multiply(1, 1), 1); ////red and not working
 
 import {assert} from 'chai';
 import {multiply} from '../multiply.js';
 
 
-describe ('Multiplier', function(){it('multiply returns product', function(){
-assert.strictEqual(multiply(1, 1), 1, "multiply(1, 1) should return 1"); //cycle 1
-assert.strictEqual(multiply(2, 2), 4, "multiply(2, 2) should return 4"); //cycle 2
-assert.strictEqual(multiply(3, 3), 9, "multiply(3, 3) should return 9"); //cycle 3
-assert.strictEqual(multiply(4, 4), 16, "multiply(4, 4) should return 16"); //cycle 4
-assert.strictEqual(multiply(23, 45), 23 * 45, "multiply(23, 45) should return 23 * 45"); //cycle 5
-    })
+describe ('Multiplier', function(){
+  
+   //cycle 1
+  it('returns 1', function() {
+assert.strictEqual(multiply(1, 1), 1, ); 
   })
+
+     //cycle 2
+     it('returns 2', function() {
+      assert.strictEqual(multiply(2, 2), 4); 
+        })
+
+        //cycle 3
+     it('returns 9', function() {
+      assert.strictEqual(multiply(3, 3), 9); 
+        })
+
+
+            //cycle 4
+     it('returns 16', function() {
+      assert.strictEqual(multiply(4, 4), 16); 
+        })
+
+              //cycle 5
+     it('returns 1035', function() {
+      assert.strictEqual(multiply(23, 45), 1035); 
+        })
+    })
