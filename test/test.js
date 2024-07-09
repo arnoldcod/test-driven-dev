3e017f0e1ab823bb0acd12c7406d62a9c3c5c0ca
KIGMAN <Valentine Kiguli>
2024-07-08 23:35:33 -0700

    Installed ESlint, removed unnecessary comments

--- test/test.js
+++ test/test.js
@@ -1,51 +1,9 @@
+// By Arnold Weasly Bolongo & Valentine Kiguli
 
-
-
-
-//MULTIPLY
-
-
-// assert.Equal(multiply(1, 1), 1); ////red and not working
-
- import {assert} from 'chai';
-
-// import {multiply} from '../multiply.js';
-
+import {assert} from 'chai';
 import {factorial} from '../factorial.js';
 import { describe } from 'mocha';
-
-
-// describe ('Multiplier', function(){
-  
-//    //cycle 1
-//   it('returns 1', function() {
-// assert.strictEqual(multiply(1, 1), 1, ); 
-//   })
-
-//      //cycle 2
-//      it('returns 2', function() {
-//       assert.strictEqual(multiply(2, 2), 4); 
-//         })
-
-//         //cycle 3
-//      it('returns 9', function() {
-//       assert.strictEqual(multiply(3, 3), 9); 
-//         })
-
-
-//             //cycle 4
-//      it('returns 16', function() {
-//       assert.strictEqual(multiply(4, 4), 16); 
-//         })
-
-//               //cycle 5
-//      it('returns 1035', function() {
-//       assert.strictEqual(multiply(23, 45), 1035); 
-//         })
-//     })
-
-
-
+import { it } from 'mocha';
 
 //FACTORIAL
 
