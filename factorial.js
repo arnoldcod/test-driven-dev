276c1e81cdb90b6f54803343f92eff4ec8de0f26
KIGMAN <Valentine Kiguli>
2024-07-09 16:36:37 -0700

    Added limits to the function and reinstalled Mocha and Chai as Dev dependencies

--- factorial.js
+++ factorial.js
@@ -1,15 +1,12 @@
+  // By Arnold Weasly Bolongo & Valentine Kiguli
+  
   export function factorial(n) {
-    if (n === 0 || n === 1) {
-      return 1;
-    }
-    return n * factorial(n - 1);   
+    if (n<0 || n>125)
+      return -1;
+        if (n === 0 || n === 1)
+        return 1;
+      return n * factorial(n - 1);
     }
   
- //  explanation of n
 
-  //  0(!) = 1 **
-  //  1(!) = 1 **
-  //  2(!) = 2 x 1
-  //  3(!) = 3 x 2 x 1
-  //  4(!) = 4 x 3 x 2 x 1
-  //  5(!) = 5 x 4 x 3 x 2 x 1
+// console.log(factorial(-1))
